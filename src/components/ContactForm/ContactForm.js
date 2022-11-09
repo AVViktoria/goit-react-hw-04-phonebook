f999f25823c9f2b5cc25b2e1db283b9cc39547f8
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-11-08 23:35:35 -0800

    refactor. to  hooks - ContactForm

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,72 +1,151 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+export default function ContactForm({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  //*  прописываем  внутри инпута   //
-  handleChange = evt => {
+  // const [number, setNumber] = useState('');
+  // console.log(inputState);
+
+  //   //*  прописываем  внутри инпута   //
+  const handleChange = evt => {
     const { name, value } = evt.currentTarget;
-    this.setState({ [name]: value });
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+
+      // this.setState({ [name]: value });
+      // setName(evt.currentTarget.value);
+    }
   };
 
-  //*  слушатель событий по кнопке  //
-  handleSubmit = evt => {
+  //   //*  слушатель событий по кнопке  //
+  const handleSubmit = evt => {
     evt.preventDefault();
-    this.props.onSubmit(this.state);
-    this.reset();
+    onSubmit({ name, number });
+    reset();
   };
-  //*  очищаем   сбрасываем   форму  //
-  reset = () => {
-    this.setState({ name: '', number: '' });
+  //   //*  очищаем   сбрасываем   форму  //
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div className="inputBox">
-          <label className="inputLabel" htmlFor={this.nameInputId}>
-            Name
-            <input
-              className="inputContent"
-              type="text"
-              value={this.state.name}
-              onChange={this.handleChange}
-              id={this.nameInputId}
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name"
-              required
-            />
-          </label>
-        </div>
-        <div className="inputBox">
-          <label className="inputLabel" htmlFor={this.numberInputId}>
-            Number
-            <input
-              className="inputContent"
-              type="tel"
-              value={this.state.number}
-              onChange={this.handleChange}
-              id={this.numberInputId}
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Number"
-              required
-            />
-          </label>
-        </div>
-        <button className="inputButton" type="submit">
-          {/* <span></span>
-          <span></span>
-          <span></span>
-          <span></span> */}
-          Add contact
-        </button>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="inputBox">
+        <label className="inputLabel">
+          Name
+          <input
+            className="inputContent"
+            type="text"
+            value={name}
+            onChange={handleChange}
+            // id={this.nameInputId}
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name"
+            required
+          />
+        </label>
+      </div>
+      <div className="inputBox">
+        <label className="inputLabel">
+          Number
+          <input
+            className="inputContent"
+            type="tel"
+            value={number}
+            onChange={handleChange}
+            // id={this.numberInputId}
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Number"
+            required
+          />
+        </label>
+      </div>
+      <button className="inputButton" type="submit">
+        Add contact
+      </button>
+    </form>
+  );
 }
-export default ContactForm;
+
+// class ContactForm extends Component {
+//   state = {
+//     name: '',
+//     number: '',
+//   };
+
+//   //*  прописываем  внутри инпута   //
+//   handleChange = evt => {
+//     const { name, value } = evt.currentTarget;
+//     this.setState({ [name]: value });
+//   };
+
+//   //*  слушатель событий по кнопке  //
+//   handleSubmit = evt => {
+//     evt.preventDefault();
+//     this.props.onSubmit(this.state);
+//     this.reset();
+//   };
+//   //*  очищаем   сбрасываем   форму  //
+//   reset = () => {
+//     this.setState({ name: '', number: '' });
+//   };
+
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <div className="inputBox">
+//           <label className="inputLabel" htmlFor={this.nameInputId}>
+//             Name
+//             <input
+//               className="inputContent"
+//               type="text"
+//               value={this.state.name}
+//               onChange={this.handleChange}
+//               id={this.nameInputId}
+//               name="name"
+//               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//               title="Name"
+//               required
+//             />
+//           </label>
+//         </div>
+//         <div className="inputBox">
+//           <label className="inputLabel" htmlFor={this.numberInputId}>
+//             Number
+//             <input
+//               className="inputContent"
+//               type="tel"
+//               value={this.state.number}
+//               onChange={this.handleChange}
+//               id={this.numberInputId}
+//               name="number"
+//               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//               title="Number"
+//               required
+//             />
+//           </label>
+//         </div>
+//         <button className="inputButton" type="submit">
+//           {/* <span></span>
+//           <span></span>
+//           <span></span>
+//           <span></span> */}
+//           Add contact
+//         </button>
+//       </form>
+//     );
+//   }
+// }
+// export default ContactForm;
