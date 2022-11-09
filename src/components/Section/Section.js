97c9df89bd67fe70c50c534d4e91e12984710b16
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-11-08 23:47:50 -0800

    refactor. to hooks - Section

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Section = ({ title, children }) => {
+export default function Section({ children }) {
   return (
     <section className="section">
       {/* {title && <h2>{title}</h2>} */}
       {children}
     </section>
   );
-};
-export default Section;
+}
 
 Section.propTypes = {
   children: PropTypes.node,
